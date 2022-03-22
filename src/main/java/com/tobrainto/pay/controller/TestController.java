b960f05f087d4d82f8dc36f1670294fd6968394b
tobrainto <tobrainto@qq.com>
2022-03-22 11:43:03 +0800

    logback and sleuth

--- src/main/java/com/tobrainto/pay/controller/TestController.java
+++ src/main/java/com/tobrainto/pay/controller/TestController.java
@@ -2,6 +2,7 @@ package com.tobrainto.pay.controller;
 
 import com.tobrainto.pay.router.PayRouter;
 import com.tobrainto.pay.router.param.PayParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/pay/router/")
+@Slf4j
 public class TestController {
 
   @Autowired
@@ -18,6 +20,7 @@ public class TestController {
   @PostMapping("/test")
   @ResponseBody
   public boolean test(PayParam param) {
+    log.info("param: {}",param);
     payRouter.selectThenApply(param);
     return Boolean.TRUE;
   }
